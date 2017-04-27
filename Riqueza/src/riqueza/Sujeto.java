f31c67799d222c6e528a1403f8bebe17367fa606
jcfalcon <jcfalcon@DESKTOP-U7PG468>
2017-04-27 01:32:05 +0100

    Definición de las clases

---
+++ Riqueza/src/riqueza/Sujeto.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package riqueza;
+
+import java.util.List;
+
+/**
+ *
+ * @author jcfalcon
+ */
+public class Sujeto {
+    int id;
+    String nombre;
+    int cartera;
+    List <Grupos> grupos;
+    List <Producto> producto;
+    List <Producto> necesidades;
+}
